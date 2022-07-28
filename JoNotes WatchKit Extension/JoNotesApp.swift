6ed3a3aba699c897045fab805ec4d257a64e6c72
joseDev <jose96corrza@gmail.com>
2022-07-27 20:53:21 -0500

    Initial Commit

---
+++ JoNotes WatchKit Extension/JoNotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  JoNotesApp.swift
+//  JoNotes WatchKit Extension
+//
+//  Created by Jose Daniel Corredor Zambrano on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct JoNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
